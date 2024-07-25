d2eb438950e5ef0d95200d2384706aa26f3713d6
Sonya <sofidesu@yandex.ru>
2024-07-25 23:37:55 +0300

    copy from main pc

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 1: Задайте значения M и N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+//Использовать рекурсию, не использовать циклы.
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Input M: ");
+        int M = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Input N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Natural numbers in the range from {M} to {N}:");
+        PrintNumbers(M, N);
+    }
+
+    static void PrintNumbers(int M, int N)
+    {
+        if (M > N)
+        {
+            return;
+        }
+
+        Console.Write(M + " ");
+        PrintNumbers(M + 1, N);
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 3: Задайте произвольный массив. 
+//Выведете его элементы, начиная с конца. 
+//Использовать рекурсию, не использовать циклы.
+
+using System;
+
+
+class Program
+
+{
+
+   static void Main(string[] args)
+
+   {
+
+       int[] array = { 3, 8, 94, 103, 180, 330 };
+
+       PrintArrayReversed(array, array.Length - 1);
+
+   }
+
+
+   static void PrintArrayReversed(int[] array, int index)
+
+   {
+
+       if (index >= 0)
+
+       {
+
+           Console.Write(array[index] + " ");
+
+           PrintArrayReversed(array, index - 1);
+
+       }
+
+   }
+
+}
